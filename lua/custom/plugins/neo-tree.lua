493f37c8b84e7b63678a28e516fd0154b5b74da3
Jan Christophersen <code@chrstphrsn.cc>
2024-03-06 17:04:13 +0100

    update plugins

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -1,6 +1,7 @@
 return {
   'nvim-neo-tree/neo-tree.nvim',
   branch = 'v3.x',
+  lazy = false,
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
@@ -12,5 +13,6 @@ return {
   },
   keys = {
     { '\\', ':Neotree reveal left<CR>', desc = 'Neotree reveal' },
+    { '<C-n>', ':Neotree toggle<CR>', desc = 'Neotree toggle' },
   },
 }
