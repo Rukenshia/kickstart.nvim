df97484bd6e310aa26d69d4240d35be5cb57f65e
Jan Christophersen <code@chrstphrsn.cc>
2024-06-01 12:40:52 +0200

    update plugins

--- lua/custom/plugins/spectre.lua
+++ lua/custom/plugins/spectre.lua
@@ -2,6 +2,6 @@ return {
   'nvim-pack/nvim-spectre',
   cmd = { 'Spectre' },
   keys = {
-    { 'n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', desc = 'Toggle Spectre' },
+    { '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', desc = 'Toggle Spectre' },
   },
 }
