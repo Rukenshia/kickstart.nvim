9c2eb90f903ce0864f831396c3b2994f136c0496
Rukenshia <code@chrstphrsn.cc>
2024-11-25 09:37:15 +0100

    fix session

---
+++ lua/custom/plugins/persisted.lua
@@ -0,0 +1,18 @@
+return {
+  'olimorris/persisted.nvim',
+  lazy = true, -- make sure the plugin is always loaded at startup
+  config = function()
+    require('persisted').setup()
+
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'PersistedSavePre',
+      callback = function()
+        for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+          if vim.bo[buf].filetype == 'neo-tree' then
+            vim.api.nvim_buf_delete(buf, { force = true })
+          end
+        end
+      end,
+    })
+  end,
+}
