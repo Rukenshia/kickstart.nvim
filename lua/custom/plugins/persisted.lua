8734c435fe430ad99fc9b6f98ffb4d97459b0976
Rukenshia <code@chrstphrsn.cc>
2025-03-17 10:03:51 +0100

    update session management

--- lua/custom/plugins/persisted.lua
+++ lua/custom/plugins/persisted.lua
@@ -7,9 +7,19 @@ return {
     vim.api.nvim_create_autocmd('User', {
       pattern = 'PersistedSavePre',
       callback = function()
+        -- close DAP UI if it's open
+        require('dapui').close()
+
         for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-          if vim.bo[buf].filetype == 'neo-tree' or vim.bo[buf].filetype == 'Outline' then
+          -- remove unnamed buffers
+          if vim.api.nvim_buf_get_name(buf) == '' then
             vim.api.nvim_buf_delete(buf, { force = true })
+            return
+          end
+
+          if vim.bo[buf].filetype == 'neo-tree' or vim.bo[buf].filetype == 'Outline' or vim.api.nvim_buf_get_name(buf):find 'neo-tree' then
+            vim.api.nvim_buf_delete(buf, { force = true })
+            return
           end
         end
       end,
