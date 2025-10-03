3e2a5c38a1fb3476618d977a36fae478c61a52f1
Rukenshia <code@chrstphrsn.cc>
2025-10-03 11:10:31 +0200

    remove nvim-dap-python

--- lua/custom/plugins/nvim-dap.lua
+++ lua/custom/plugins/nvim-dap.lua
@@ -4,20 +4,16 @@ return {
     dependencies = {
       'nvim-neotest/nvim-nio',
       'rcarriga/nvim-dap-ui',
-      'mfussenegger/nvim-dap-python',
       'theHamsta/nvim-dap-virtual-text',
     },
     config = function()
       local dap = require 'dap'
       local dapui = require 'dapui'
-      local dap_python = require 'dap-python'
 
       require('dapui').setup {}
       require('nvim-dap-virtual-text').setup {
         commented = true, -- Show virtual text alongside comment
       }
-
-      dap_python.setup 'python3'
 
       vim.fn.sign_define('DapBreakpoint', {
         text = '',
